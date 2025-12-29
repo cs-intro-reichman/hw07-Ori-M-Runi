8bec867222c8f6549e881cc46a0c5b554d134380
Ori-M-Runi <ori.montuellemanna@post.runi.ac.il>
2025-12-29 18:41:54 +0200

    Initial commit

--- Koch.java
+++ Koch.java
@@ -17,18 +17,41 @@ public class Koch {
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
 		*/
 
-		/*
+		
 		// Tests the snowflake function:
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
 		snowFlake(Integer.parseInt(args[0]));
-		*/
+		
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
 		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+
+		if (n > 1) {
+			// Calculates the triangle point (x0, y0) and the one and two thirds points of the line (x3, y3), (x4, y4)
+			double x0 = (Math.sqrt(3) / 6.0) * (y1 - y2) + 0.5 * (x1 + x2);
+			double y0 = (Math.sqrt(3) / 6.0) * (x2 - x1) + 0.5 * (y1 + y2);
+			double x3 = (2*x1 + x2)/3, x4 = (x1 + 2*x2)/3;
+			double y3 = (2*y1 + y2)/3, y4 = (y1 + 2*y2)/3;
+			
+			// Draws out the base of the triangle
+			StdDraw.setPenColor(StdDraw.WHITE);
+			//StdDraw.line(x3, y3, x4, y4);
+			StdDraw.line(x1, y1, x2, y2);
+
+			// Draws the sides of the triangle
+			StdDraw.setPenColor(StdDraw.BLACK);
+			StdDraw.line(x3, y3, x0, y0);
+			StdDraw.line(x0, y0, x4, y4);
+
+			// calls the function again for each one of the four segments
+			curve(n - 1, x1, y1, x3, y3);
+			curve(n - 1, x3, y3, x0, y0);
+			curve(n - 1, x0, y0, x4, y4);
+			curve(n - 1, x4, y4, x2, y2);
+		}
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
@@ -37,6 +60,12 @@ public class Koch {
 		StdDraw.setYscale(0, 1.1);
 		StdDraw.setXscale(0, 1.1);
 		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		double x1 = 0.05, x2 = 1.05;
+		double y1 = 0.8, y2 = 0.8;
+		double x3 = 0.55;
+		double y3 = 0.02;
+		curve(n, x1, y1, x2, y2);
+		curve(n, x3, y3, x1, y1);
+		curve(n, x2, y2, x3, y3);
 	}
 }
